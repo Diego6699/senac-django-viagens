3ce6b17ab7b43a7b83ea6d4887db3b94971d8da1
DIEGO LEONARDO DA SILVA FRANÇA <diegoleonardo9966@gmail.com>
2022-10-20 21:13:53 -0300

    Validando form

--- viagens/views.py
+++ viagens/views.py
@@ -1,5 +1,6 @@
 from django import views
 from django.shortcuts import render
+from django.template import Origin
 from viagens.forms import ViagemForms
 
 # Create your views here.
@@ -8,9 +9,21 @@ def index(request):
     form = ViagemForms()
     contexto = {'form':form}
     return render(request ,'index.html',contexto)
-
+'''
 def revConsulta(request):
     if request.method == 'POST':
         form = ViagemForms(request.POST)
         contexto = {'form' : form}
-        return render(request, 'consulta.html',contexto)
+        return render(request, 'consulta.html',contexto) 
+'''
+        
+def revConsulta(request):
+    if request.method == 'POST':
+        form = ViagemForms(request.POST)
+        if form.is_valid():
+            contexto = {'form':form}#erro
+            return render(request,'consulta.html',contexto)
+        else:
+            print('form inválido')
+            contexto = {'form':form}
+            return render(request,'index.html',contexto)
