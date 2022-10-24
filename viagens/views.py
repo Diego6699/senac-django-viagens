f88fd3886a98408aeca4073866fb259e9c85cc6a
Diego Leonardo da Silva França <diegoleonardo9966@gmail.com>
2022-10-24 16:49:05 -0300

    correção do formularios

--- viagens/views.py
+++ viagens/views.py
@@ -1,30 +1,19 @@
-from django import views
 from django import forms
 from django.shortcuts import render
-from django.template import Origin
 from viagens.forms import ViagemForms
 
-# Create your views here.
-
 def index(request):
-    form = ViagemForms()
-    contexto = {'form':form}
-    return render(request ,'index.html',contexto)
-'''
+    form= ViagemForms()
+    contexto={'form': form}
+    return render(request, 'index.html', contexto)
+
 def revConsulta(request):
-    if request.method == 'POST':
-        form = ViagemForms(request.POST)
-        contexto = {'form' : form}
-        return render(request, 'consulta.html',contexto) 
-'''
-        
-def revConsulta(request):
-    if request.method == 'POST':
+    if request.method =='POST':
         form = ViagemForms(request.POST)
         if form.is_valid():
-            contexto = {'form':form}#erro
-            return render(request,'consulta.html',contexto)
-        else:
-            print('form inválido')
-            contexto = {'form':form}
-            return render(request,'index.html',contexto)
+            contexto={'form': form}
+            return render(request, 'consulta.html', contexto)
+        else: 
+            print('Form inválido')
+            contexto = {'form': form}
+            return render(request, 'index.html', contexto)
